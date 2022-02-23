ad0828e2d17b2f4ffc630f4bc988932cab38a0a5
Lorenzo Barretta <43916566+devlbd@users.noreply.github.com>
2022-02-23 19:26:22 +0100

    Changing name.

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -15,7 +15,7 @@ public class GameFrame extends JFrame {
 		
 		this.getContentPane().add(panel); //finestra Swing
 		
-		this.setTitle("Breakout");
+		this.setTitle("Bricks Crusher");
 		this.setResizable(false);
 		this.setBackground(Color.black);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
